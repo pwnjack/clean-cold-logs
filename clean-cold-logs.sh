85f4c0c8f4c7a3c808a5697498da00a791c6ee48
pwnjack <pwnjack@gmail.com>
2022-10-03 10:52:00 +0200

    code cleanup

--- clean-cold-logs.sh
+++ clean-cold-logs.sh
@@ -5,11 +5,11 @@ echo  "### Splunk Cleanup Cold Logs ###"
 echo  "###########   2022   ###########"
 echo  "################################"
 ##############
-echo -n " Enter index name to lookup:"
+echo -n "Enter index name to lookup:"
 read INAME
 ####
 FROZENPATH="/opt/splunk/var/lib/splunk"
-echo " Default Splunk Frozen Indexes Path is "$FROZENPATH". is it ok? (y to continue or n to give new path):"
+echo "Default Splunk Frozen Indexes Path is "$FROZENPATH". is it ok? (y to continue or n to give new path):"
 read  ANSWER1;
 case "$ANSWER1" in
 "y")
@@ -37,36 +37,36 @@ BODATE=$(date --date="`date`-"$ODATE"days" +%s)
 BCDATE=`date +%s`
 #############
 FILE1='./frozendb.txt'
- while read line; do
-           LOGSTART=`echo $line | cut -d "_" -f3`;
-	             LOGEND=`echo $line | cut -d "_" -f2`;
-		     if [[ $LOGEND -gt $BCDATE || $LOGSTART -lt $BODATE ]]; then
-		     echo -e "******************************"
-		     echo -e "Frozen Log Path You want: $line"
-		     HLOGSTART=`date -d @"$LOGSTART"`
-		     HLOGEND=`date -d @"$LOGEND"`
-		     LOGSIZE=`du -hs "$line" | cut -d "/" -f1`
-		     echo -e "*** this Bucket contains logs from: $HLOGSTART"
-		     echo -e "*** this Bucket contains logs to: $HLOGEND "
-		     echo -e "**** The Size Of This Log Is: $LOGSIZE"
-		     echo -e "$line" >> "./frozenmatched.txt"
-		     echo -e "******************************"
-		     fi
-		     done<$FILE1
-		     ############
-		     sudo rm -rf "./frozendb.txt"
-		     echo "Do you Want to DELETE this Logs?(y to DELETE): "
-		     read  ANSWER3
-		     FILE2='./frozenmatched.txt'
-		     if [[ "$ANSWER3" == "y" ]]; then
-		     while read line2; do
-		             sudo rm -rf "$line2"
-			             echo -e "DELETING of $line2 DONE."
-				     done<$FILE2
-				     fi
-				     sudo rm -rf "./frozenmatched.txt"
-				     ##########
-				     echo     "################################"
-				     echo  -e "########## COMPLETED ###########"
-				     echo     "################################"
-				     #########
+while read line; do
+	LOGSTART=`echo $line | cut -d "_" -f3`;
+	LOGEND=`echo $line | cut -d "_" -f2`;
+	if [[ $LOGEND -gt $BCDATE || $LOGSTART -lt $BODATE ]]; then
+		echo -e "******************************"
+		echo -e "Frozen Log Path You want: $line"
+		HLOGSTART=`date -d @"$LOGSTART"`
+		HLOGEND=`date -d @"$LOGEND"`
+		LOGSIZE=`du -hs "$line" | cut -d "/" -f1`
+		echo -e "*** this Bucket contains logs from: $HLOGSTART"
+		echo -e "*** this Bucket contains logs to: $HLOGEND "
+		echo -e "**** The Size Of This Log Is: $LOGSIZE"
+		echo -e "$line" >> "./frozenmatched.txt"
+		echo -e "******************************"
+	fi
+done<$FILE1
+############
+sudo rm -rf "./frozendb.txt"
+echo "Do you Want to DELETE this Logs?(y to DELETE): "
+read  ANSWER3
+FILE2='./frozenmatched.txt'
+	if [[ "$ANSWER3" == "y" ]]; then
+		while read line2; do
+			sudo rm -rf "$line2"
+			echo -e "DELETING of $line2 DONE."
+		done<$FILE2
+	fi
+sudo rm -rf "./frozenmatched.txt"
+##########
+echo     "################################"
+echo  -e "########## COMPLETED ###########"
+echo     "################################"
+#########
